aa2ff599e98caa1f30119d576d47bd349ec1745f
leshax1 <privsupport@gmail.com>
2024-08-01 13:41:10 +0700

    args

--- app/main.py
+++ app/main.py
@@ -13,15 +13,15 @@ logger.info("FastAPI application starting up...")
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate,  db = Depends(database.get_db)):
-    return crud.create_user(conn=db, user=user)
+    return crud.create_user(db, user)
 
 @app.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 10, db = Depends(database.get_db)):
-    return crud.get_users(conn=db, skip=skip, limit=limit)
+    return crud.get_users(db, skip, limit)
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db = Depends(database.get_db)):
-    user = crud.get_user(conn=db, user_id=user_id)
+    user = crud.get_user(db, user_id)
     if user is None:
         logger.error(f"User with ID {user_id} not found")
         raise HTTPException(status_code=404, detail="User not found")
@@ -29,11 +29,11 @@ def read_user(user_id: int, db = Depends(database.get_db)):
 
 @app.put("/users/{user_id}", response_model=schemas.User)
 def update_user(user_id: int, user: schemas.UserCreate, db = Depends(database.get_db)):
-    return crud.update_user(conn=db, user_id=user_id, user=user)
+    return crud.update_user(db, user_id, user)
 
 @app.delete("/users/{user_id}", response_model=schemas.User)
 def delete_user(user_id: int, db = Depends(database.get_db)):
-    deleted_user = crud.delete_user(conn=db, user_id=user_id)
+    deleted_user = crud.delete_user(db, user_id)
 
     if deleted_user is None: 
         logger.error(f"User with ID {user_id} not found")
@@ -43,7 +43,7 @@ def delete_user(user_id: int, db = Depends(database.get_db)):
 
 @app.post("/users/{user_id}/posts/", response_model=schemas.Post)
 def create_post_for_user(user_id: int, post: schemas.PostCreate, db = Depends(database.get_db)):
-    post = crud.create_post(conn=db, post=post, user_id=user_id)
+    post = crud.create_post(db, post, user_id)
 
     if post is None:
         logger.error(f"Can not create post")
@@ -53,11 +53,11 @@ def create_post_for_user(user_id: int, post: schemas.PostCreate, db = Depends(da
 
 @app.get("/posts/", response_model=list[schemas.Post])
 def read_posts(skip: int = 0, limit: int = 10, db = Depends(database.get_db)):
-    return crud.get_posts(conn=db, skip=skip, limit=limit)
+    return crud.get_posts(db, skip, limit)
 
 @app.get("/posts/{post_id}", response_model=schemas.Post)
 def read_post(post_id: int, db = Depends(database.get_db)):
-    post = crud.get_post(conn=db, post_id=post_id)
+    post = crud.get_post(db, post_id)
 
     if post is None:
         logger.error(f"Post with ID {post_id} not found")
@@ -66,7 +66,7 @@ def read_post(post_id: int, db = Depends(database.get_db)):
 
 @app.put("/posts/{post_id}", response_model=schemas.Post)
 def update_post(post_id: int, post: schemas.PostCreate, db = Depends(database.get_db)):
-    post = crud.update_post(conn=db, post_id=post_id, post=post)
+    post = crud.update_post(db, post_id, post)
 
     if post is None:
         logger.error(f"Post with ID {post_id} not found")
@@ -75,7 +75,7 @@ def update_post(post_id: int, post: schemas.PostCreate, db = Depends(database.ge
 
 @app.delete("/posts/{post_id}", response_model=schemas.Post)
 def delete_post(post_id: int, db = Depends(database.get_db)):
-    post = crud.delete_post(conn=db, post_id=post_id)
+    post = crud.delete_post(db, post_id)
 
     if post is None:
         logger.error(f"Post with ID {post_id} not found")
@@ -84,6 +84,6 @@ def delete_post(post_id: int, db = Depends(database.get_db)):
 
 @app.get("/users/{user_id}/posts/", response_model=list[schemas.Post])
 def read_posts_by_user(user_id: int, db = Depends(database.get_db)):
-    posts = crud.get_posts_by_user(conn=db, user_id=user_id)
+    posts = crud.get_posts_by_user(db, user_id)
     return posts
 
